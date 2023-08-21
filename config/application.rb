0ea3f55b4857049c8d1335c869e987f008eea2c8
JARG85 <jahir85rodriguez@hotmail.com>
2023-08-21 13:34:58 -0500

    Refactor: change several files for add all responsive crud

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module QuoteEditor
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.available_locales = [:en, :es]
-    config.i18n.default_locale = :es
+    config.i18n.default_locale = :en
   end
 end
